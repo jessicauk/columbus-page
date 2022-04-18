e36a2243452650ee1338c9d743d258a9e815f39f
Jessica Apolinar <jessica_220691@hotmail.com>
2022-04-18 13:54:25 -0500

    charts

--- components/columnchart/columnchart.tsx
+++ components/columnchart/columnchart.tsx
@@ -1,57 +1,76 @@
-import styles from './columnchart.module.css'
-import Highcharts from 'highcharts';
-import HighchartsReact from 'highcharts-react-official';
-
-const options = {
-  
-  chart: {
-    type: 'column',
-  },
-
-  title: {
-    text: ''
-},
-
-  xAxis: {
-    categories: ['Jun', 'Jul', 'Ago', 'Sept', 'Oct', 'Nov']
-  },
-
-  yAxis: {
-    allowDecimals: false,
-    min: 0,
-  },
-
-  tooltip: {
-    formatter: function () {
-      return '<b>' + this.x + '</b><br/>' +
-        this.series.name + ': ' + this.y + '<br/>' +
-        'Total: ' + this.point.stackTotal;
-    }
-  },
-
-  plotOptions: {
-    column: {
-      stacking: 'normal'
-    }
-  },
-
-  series: [{
-    name: '',
-    data: [0.5, 0.5, 0.5, 0.5, 0.5, 0.5],
-    color: '#47ddfa',
-    // stack: 'male'
-  }, {
-    name: '',
-    data: [10, 10, 10, 10, 10, 12],
-    color: '#0547d0',
-    // stack: 'male'
-  }]
-};
 
 export default function ColumnChart() {
   return (
-    <div className={styles.barchart}>
-      <HighchartsReact highcharts={Highcharts} options={options} />
+    <div className="barchart">
+      <div className="barchart__container vertical flat">
+        <div className="progress-bar">
+        <div className="tooltip">
+            <span className="tooltiptext">$1,234.00 MXN</span>
+          </div>
+          <div className="progress-track">
+           
+            <div className="progress-fill">
+            </div>
+          </div>
+          <span>Jun</span>
+          
+        </div>
+
+        <div className="progress-bar">
+        <div className="tooltip">
+            <span className="tooltiptext">$1,234.00 MXN</span>
+          </div>
+          <div className="progress-track">
+            <div className="progress-fill">
+            </div>
+          </div>
+          <span>Jul</span>
+        </div>
+
+        <div className="progress-bar">
+        <div className="tooltip">
+            <span className="tooltiptext">$1,234.00 MXN</span>
+          </div>
+          <div className="progress-track">
+            <div className="progress-fill">
+            </div>
+          </div>
+          <span>Ago</span>
+        </div>
+
+        <div className="progress-bar">
+        <div className="tooltip">
+            <span className="tooltiptext">$1,234.00 MXN</span>
+          </div>
+          <div className="progress-track">
+            <div className="progress-fill">
+            </div>
+          </div>
+          <span>Sep</span>
+        </div>
+
+        <div className="progress-bar">
+        <div className="tooltip">
+            <span className="tooltiptext">$1,234.00 MXN</span>
+          </div>
+          <div className="progress-track">
+            <div className="progress-fill">
+            </div>
+          </div>
+          <span>Oct</span>
+        </div>
+
+        <div className="progress-bar">
+        <div className="tooltip">
+            <span className="tooltiptext">$1,234.00 MXN</span>
+          </div>
+          <div className="progress-track">
+            <div className="progress-fill">
+            </div>
+          </div>
+          <span>Nov</span>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
